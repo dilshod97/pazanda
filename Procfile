b8a5eff38d3e18cb14e8b70dae90bcf1f13b08e6
dil <dilshodbaxriddinov97@gmail.com>
2023-04-05 14:14:34 +0500

    s

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn 'pazanda.wsgi'
--- pazanda/settings.py
+++ pazanda/settings.py
@@ -107,15 +107,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-TOKEN = '6298524151:AAHDErLe-kWTWpqcHxtTcDny6sjaYuhmJXo'
 STATIC_URL = '/static/'
-if DEBUG:
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, "static"),
-    )
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#
+TOKEN = '6298524151:AAHDErLe-kWTWpqcHxtTcDny6sjaYuhmJXo'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
